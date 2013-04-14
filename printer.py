dbe000d05c7ed12c74070e2487e5bd372b36579d
bodiedoo <bodiedoo@aol.com>
2013-04-13 21:11:01 -0700

    Update printer.py

--- printer.py
+++ printer.py
@@ -167,35 +167,35 @@ class ThermalPrinter(object):
         self.printer.write(self._ESC)
         self.printer.write(chr(123))
         self.printer.write(chr(1))
-    #######################################################################
-    #							BARCODE								      #
+	#######################################################################
+	#				BARCODE				      #
 	#Please read http://www.adafruit.com/datasheets/A2-user%20manual.pdf  #
-	#for information on how to use barcodes								  #
+	#for information on how to use barcodes	 			      #
 	#######################################################################
 	
-	def barcode_chr(self, MSG):
+    def barcode_chr(self, MSG):
         self.printer.write(chr(29)) """Leave"""
         self.printer.write(chr(72)) """Leave"""
         self.printer.write(msg) """ Print barcode # 1:Abovebarcode 2:Below 3:Both 0:Not printed """
 		
-	def barcode_height(self):
+    def barcode_height(self):
         self.printer.write(chr(29)) """Leave"""
         self.printer.write(chr(104)) """Leave"""
         self.printer.write(msg) """ Value 1-255 Default 50 """
 		
-	def barcode_height(self):
+    def barcode_height(self):
         self.printer.write(chr(29)) """Leave"""
         self.printer.write(chr(119)) """Leave"""
         self.printer.write(chr(2)) """ Value 2,3 Default 2 """
-	####################################
-	#CODE SYSTEM, NUMBER OF CHARACTERS #
+	############################################
+	#CODE SYSTEM, NUMBER OF CHARACTERS 	   #
 	#65=UPC-A	11,12	#71=CODEBAR	>1 #
 	#66=UPC-E	11,12	#72=CODE93	>1 #
 	#67=EAN13	12,13	#73=CODE128	>1 #
-	#68=EAN8	7,8		#74=CODE11	>1 #
-	#69=CODE39	>1		#75=MSI		>1 #
+	#68=EAN8	7,8	#74=CODE11	>1 #
+	#69=CODE39	>1	#75=MSI		>1 #
 	#70=I25		>1 EVEN NUMBER		   #
-	####################################
+	############################################
 		
     def barcode(self, msg):
         self.printer.write(chr(29)) """ LEAVE """
@@ -203,7 +203,7 @@ class ThermalPrinter(object):
         self.printer.write(chr(65)) """ USE ABOVE CHART """
         self.printer.write(chr(12)) """ USE CHART NUMBER OF CHAR """
         self.printer.write(msg)
-      #############
+      	  #############
 	  #END BARCODE#
 	  #############
         
